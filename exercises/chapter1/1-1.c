c3e200082cea0b536aee7f402d8e8a232404903c
JazzerThighs <profenjb199@gmail.com>
2023-11-06 22:35:17 -0500

    new file:   exercises/chapter1/1-1.c 	new file:   exercises/chapter1/1-1.exe 	new file:   exercises/chapter1/1-2.c 	new file:   exercises/chapter1/1-2.exe

---
+++ exercises/chapter1/1-1.c
@@ -0,0 +1,5 @@
+#include <stdio.h>                  //include information about standard library
+void main()                              //define a function called main that received no argument values
+{                                   //statements of main are enclosed in braces
+        printf("hello, world!\n");   //main calls library function printf to print this sequence of characters
+}                                   //\n represents the newline character
---
+++ exercises/chapter1/1-2.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+void main()
+{
+    printf("hello, world!\n");
+    printf("\t\n");
+    printf("\b\n");
+    printf("\"\n");
+    printf("\\\n");
+}
